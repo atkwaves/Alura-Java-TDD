28e49e0db055b11cc3fcedc2294412dd9084daab
atk <atkwaves@gmail.com>
2022-04-23 12:13:55 -0300

    A02 JUnit

---
+++ JUnit/src/br/com/alura/tdd/CalculadoraTest.java
@@ -0,0 +1,18 @@
+package br.com.alura.tdd;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+
+
+public class CalculadoraTest {
+
+	@Test
+	public void deveriaSomarDoisNumerosPositivos() {
+		
+		Calculadora calculadora = new Calculadora();
+		int soma = calculadora.somar(3, 7);
+		
+		Assert.assertEquals(10, soma);
+	}
+}
